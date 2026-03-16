3556af06716e219394daef76472f1fc46a96281d
armandojunior007 <armandojunior007@gmail.com>
2026-03-16 12:13:57 +0100

    Update book details and output message in LibraryApp
    git add E2-library-buggy/LibraryApp.java
    git commit -m "fix: actualizar LibraryApp con comentarios del profesor"
    
    Signed-off-by: armandojunior007 <armandojunior007@gmail.com>

--- E2-library-buggy/LibraryApp.java
+++ E2-library-buggy/LibraryApp.java
@@ -5,10 +5,10 @@ public class LibraryApp {
         Library library = new Library();
         
         // Inicializamos la biblioteca
-        library.addBook(new Book("Clean Code", "Robert Martin", "978-0132350884"));
-        library.addBook(new Book("The Pragmatic Programmer", "Hunt & Thomas", "978-0201616224"));
+        library.addBook(new Book("Clean Code", "Robert Martin", "978-0132350775"));
+        library.addBook(new Book("The Pragmatic Programmer", "Hunt & Thomas", "978-0201616115"));
         
-        System.out.println("✅ Biblioteca lista con 2 libros");
+        System.out.println("✅ Biblioteca lista con 3 libros");
         
         Book found = library.findBookByTitle("Clean Code");
         System.out.println(found);
